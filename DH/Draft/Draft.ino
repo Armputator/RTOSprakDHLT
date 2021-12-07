23d1644ff8f271ea6a5cce989b9e495f30ed2b91
D Hainsch <david.hainsch@gmx.de>
2021-12-07 22:31:45 +0100

    stuff that doesnt work

--- DH/Draft/Draft.ino
+++ DH/Draft/Draft.ino
@@ -1,8 +1,49 @@
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+#define NUM_TASKS 10
+
+// define two tasks for Blink & AnalogRead
+void Task(void *argv);
+void task_func(int n)
+{
+    Serial.println(n);
+    return;
+};
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+  
+  for(int i = 0;i<NUM_TASKS;++i)
+  {
+    xTaskCreatePinnedToCore(
+      Task
+      ,  "Task"   // A name just for humans
+      ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
+      ,  NULL
+      ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      ,  NULL 
+      ,  ARDUINO_RUNNING_CORE);
+  }
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
 
-}
+  void Task(void *argv)  // This is a task.
+  {
+    ets_delay_us(1000);
+    task_func(i);
+  }
