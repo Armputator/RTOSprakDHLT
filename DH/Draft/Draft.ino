631cd2b656dee582594e95d850159319cf30d798
D Hainsch <david.hainsch@gmx.de>
2021-12-08 11:51:32 +0100

    stuff

--- DH/Draft/Draft.ino
+++ DH/Draft/Draft.ino
@@ -12,41 +12,46 @@
 
 // define two tasks for Blink & AnalogRead
 void Task(void *pvParameters);
-void task_func(int n)
+void task_func(void *argc)
 {
-    Serial.println(n);
+    Serial.println(*(int *)argc);
     return;
 };
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  
+//int main(void){
+void setup(){
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
-  int arr[NUM_TASKS];
+ int arr[NUM_TASKS];
   
-  for(int i = 0;i<NUM_TASKS;++i)
+  for(int i = 0;i<NUM_TASKS;i++)
   {
     arr[i] = i;
+    Serial.print("i:");Serial.print(i,DEC);Serial.print("|a:");Serial.println(arr[i],DEC);
     xTaskCreatePinnedToCore(
       Task
       ,  "Task"   // A name just for humans
       ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-      ,  (arr + i)
+      ,  (void *)(arr + i)
       ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
       ,  NULL 
       ,  ARDUINO_RUNNING_CORE);
   }
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+  
+  vTaskStartScheduler();
 }
 
+void loop(){
+};
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-  void Task(void *argv)  // This is a task.
+  void Task(void *pvParameters)  // This is a task.
   {
-    (void) pvParameters;
-    ets_delay_us(1000);
-    task_func(i);
+    for(;;){
+      (void) pvParameters;
+      ets_delay_us(1000);
+      task_func(pvParameters);
+    }
   }
