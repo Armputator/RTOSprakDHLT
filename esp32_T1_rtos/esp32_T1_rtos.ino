0aec3a77fc703d620c08f8bb47e9236e6b8c0e31
D Hainsch <david.hainsch@gmx.de>
2021-12-08 19:33:21 +0100

    Teil 1 almost finished

--- esp32_T1_rtos/esp32_T1_rtos.ino
+++ esp32_T1_rtos/esp32_T1_rtos.ino
@@ -4,59 +4,89 @@
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
-
 #define NUM_TASKS 10
 
-// define two tasks for Blink & AnalogRead
-void Task(void *pvParameters);
-void task_func(void *pvParameters)
-{
-    ets_delay_us(3000);
-    Serial.println(":]");
-    //Serial.println(*argc);
-    return;
-};
+//-------------------------------
 
-//int main(void){
+uint32_t counter[NUM_TASKS] = {0};
+unsigned long lastincrease = 0;
+unsigned long lastoutput = 0;
+
+//-------------------------------
+
+void Task(void *pvParameters);
+ 
+void task_func(void *pvParameters);
+
+void Puts(void *pvParameters);
+
+//-------------------------------
+
 void setup(){
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
   
   uint8_t arr[NUM_TASKS];
-  int i = 0;
   
-  while(i < NUM_TASKS)
+  for(int i = 0;i<NUM_TASKS;i++)
   {
     arr[i] = i;
     Serial.print("i:");Serial.print(i,DEC);Serial.print("|a:");Serial.println(arr[i],DEC);
     
-    xTaskCreatePinnedToCore(
-      Task
-      ,  "Task"   // A name just for humans
-      ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-      ,  (void *)(arr + i)
-      ,  0  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-      ,  NULL 
-      ,  ARDUINO_RUNNING_CORE);
-    i++;
+    xTaskCreatePinnedToCore(Task, "Task" + i, 2048, (void *)(arr + i), 1, NULL,  ARDUINO_RUNNING_CORE);
   }
+  xTaskCreatePinnedToCore(Puts, "Output", 2048, NULL, 2, NULL,  ARDUINO_RUNNING_CORE);
   
-  vTaskStartScheduler();
-}
+  Serial.println("Setup Done!");
+} void loop(){};
 
-void loop(){};
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-  void Task(void *pvParameters)  // This is a task.
-  {
+void task_func(void *pvParameters)
+{
+    //ets_delay_us(3000);
+    Serial.println(":]");
+    
+    if(micros() - lastincrease >= 1000)
+    {
+      (*(uint32_t *)pvParameters)++;
+      lastincrease = micros();
+    }
+    return;
+};
+
+void func(void){  Serial.println(":]"); }; //unnecessary if task_func works
+
+void Task(void *pvParameters)
+{
     uint8_t *k;
-    k = (uint8_t *) pvParameters;
-    ets_delay_us(1000);
-      task_func(k);
-    for(;;){}
-  }
+    k = (uint8_t *)pvParameters;
+    uint32_t *taskcounter = counter + *k;
+    for(;;)
+    {
+      //ets_delay_us(1000);
+      //func();
+      task_func((void *)taskcounter);
+    }
+};
+
+void Puts(void *pvParameters)
+{
+  vTaskDelay(100);
+  for(;;)
+  {
+    if(millis() - lastoutput >= 1000)
+    {
+      for(int i = 0;i < NUM_TASKS;i++)
+      {
+        Serial.print("Counter ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(*(counter+i));
+      }
+      lastoutput = millis();
+    };
+  };
+};
