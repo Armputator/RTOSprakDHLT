2c5d7f28c2b3e7c33a840c88993da37bc01a6f31
D Hainsch <david.hainsch@gmx.de>
2021-12-08 10:55:40 +0100

    stuff

--- DH/Draft/Draft.ino
+++ DH/Draft/Draft.ino
@@ -11,7 +11,7 @@
 #define NUM_TASKS 10
 
 // define two tasks for Blink & AnalogRead
-void Task(void *argv);
+void Task(void *pvParameters);
 void task_func(int n)
 {
     Serial.println(n);
@@ -23,14 +23,16 @@ void setup() {
   
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
+  int arr[NUM_TASKS];
   
   for(int i = 0;i<NUM_TASKS;++i)
   {
+    arr[i] = i;
     xTaskCreatePinnedToCore(
       Task
       ,  "Task"   // A name just for humans
       ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-      ,  NULL
+      ,  (arr + i)
       ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
       ,  NULL 
       ,  ARDUINO_RUNNING_CORE);
@@ -44,6 +46,7 @@ void setup() {
 
   void Task(void *argv)  // This is a task.
   {
+    (void) pvParameters;
     ets_delay_us(1000);
     task_func(i);
   }
