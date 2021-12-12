629be28173d775d613816240ba65cbc26c5458d0
D Hainsch <david.hainsch@gmx.de>
2021-12-12 15:36:41 +0100

    draft for t2

---
+++ esp32_T2_rtos/esp32_T2_rtos.ino
@@ -0,0 +1,118 @@
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+#define NUM_TASKS 5
+#define OUTPUT_PERIOD 1 //in seconds
+#define TIMER_TASK_PRIO 10 //priority integer
+
+//-------------------------------
+
+uint32_t counter[NUM_TASKS] = {0};
+unsigned long lastincrease = 0;
+unsigned long lastoutput = 0;
+volatile TimerHandle_t output_tmr;
+TaskHandle_t timer_task = xTimerGetTimerDaemonTaskHandle();
+
+//-------------------------------
+
+void Task(void *pvParameters);
+ 
+void task_func(void *pvParameters);
+
+void Puts(void *pvParameters);
+
+void PutsTMR_callback(TimerHandle_t output_tmr); 
+
+//-------------------------------
+
+void setup(){
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+  
+  uint8_t arr[NUM_TASKS]; // array 
+  
+  for(int i = 0;i<NUM_TASKS;i++)
+  {
+    arr[i] = i;
+    //Serial.print("i:");Serial.print(i,DEC);
+
+    xTaskCreatePinnedToCore(Task, "Task" + i, 2048, (void *)(arr + i), 1, NULL,  ARDUINO_RUNNING_CORE); //all tasks priority 1
+    //xTaskCreatePinnedToCore(Task, "Task" + i, 2048, (void *)(arr + i), i, NULL,  ARDUINO_RUNNING_CORE); //tasks increasing priority, 0 to NUM_TASKS - 1
+    //xTaskCreatePinnedToCore(Task, "Task" + i, 2048, (void *)(arr + i), (i == 0 ? 1 : (i % 2 == 0 ? 1 : 2)), NULL,  ARDUINO_RUNNING_CORE); //tasks alternating priority between 1 and 2
+    
+    Serial.print("|a:");Serial.println(arr[i],DEC);
+  }
+  
+  //xTaskCreatePinnedToCore(Puts, "Output", 2048, NULL, NUM_TASKS, NULL,  ARDUINO_RUNNING_CORE);  //priority NUM_TASKS
+  //xTaskCreatePinnedToCore(Puts, "Output", 2048, NULL, 1, NULL,  ARDUINO_RUNNING_CORE);  //priority 1
+  xTaskCreatePinnedToCore(Puts, "Output", 2048, NULL, NUM_TASKS, NULL,  ARDUINO_RUNNING_CORE);  //priority 3
+
+  output_tmr = xTimerCreate();
+  vTaskPrioritySet(timer_task, TIMER_TASK_PRIO);
+  
+  Serial.println("Setup Done!");
+} void loop(){}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void PutsTMR_callback( TimerHandle_t output_tmr)
+{
+  
+}
+
+void task_func(void *pvParameters)
+{
+  if(true)
+  {
+    ets_delay_us(1000);
+    //Serial.println(":]");
+    if(micros() - lastincrease >= 1000)
+    {
+      (*(uint32_t *)pvParameters)++;
+      lastincrease = micros();
+    }
+    return;
+  }
+}
+
+void Task(void *pvParameters)
+{
+  if(true)
+  {
+    uint8_t *k;
+    k = (uint8_t *)pvParameters;
+    uint32_t *taskcounter = counter + *k;
+    vTaskDelay(1000);
+    for(;;)
+    {
+      //ets_delay_us(1000);
+      //func();
+      task_func((void *)taskcounter);
+      vTaskDelay(1 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void Puts(void *pvParameters)
+{
+  vTaskDelay(1000);
+  for(;;)
+  {
+    {
+      for(int i = 0;i < NUM_TASKS;i++)
+      {
+        Serial.print("Counter ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(*(counter+i));
+      }
+      lastoutput = millis();
+    }
+    vTaskSuspend(NULL);
+  }
+}
